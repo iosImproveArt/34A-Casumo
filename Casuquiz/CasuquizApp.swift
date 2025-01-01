d92feb83649def1ac8ad679aa4b0fb5f8c17d96f
iosImproveArt <impiosart@gmail.com>
2025-01-01 15:52:34 +0200

    init

---
+++ Casuquiz/CasuquizApp.swift
@@ -0,0 +1,17 @@
+//
+//  CasuquizApp.swift
+//  Casuquiz
+//
+//  Created by Improve on 01.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct CasuquizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
